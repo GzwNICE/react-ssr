55fedf753c7c8b0e9026947328d7f9eefa9a5d42
GzwNice <gzwnice@163.com>
2018-12-18 17:18:59 +0800

    feat: 首页判断登录显示头像接口完成

--- src/actions/userStatus.js
+++ src/actions/userStatus.js
@@ -3,11 +3,13 @@
  */
 import { singleApi } from '../helper/reduxFetch'
 export const GET_USER_STATUS = 'GET_USER_STATUS' // 获取用户状态
+export const GET_USER_LOGIN = 'GET_USER_LOGIN' // 获取用户状态(新)
 export const USER_REFERSH_RESUME = 'USER_REFERSH_RESUME' // 刷新简历
 export const LOGIN_OUT = 'LOGIN_OUT' // 登出
 export const SAVE_USER_CITY_CODE = 'SAVE_USER_CITY_CODE' // 储存用户选择的城市id
 
 const URLHOT = ':ve.mobile.interface/user/status'
+const LOGGIN = ':ve.mobile.interface/user/photo'
 const RESUME = ':ve.mobile.interface/resume/set_base'
 
 export const getUserStatus = singleApi({
@@ -19,6 +21,18 @@ export const getUserStatus = singleApi({
     }
   },
 })
+
+// 获取当前登录状态（带头像） 
+export const loggingStatus = singleApi({
+  url: LOGGIN,
+  action: (args, json) => {
+    return {
+      type: GET_USER_LOGIN,
+      data: json.data,
+    }
+  },
+})
+
 
 export const userRefResume = singleApi({
   url: RESUME,
