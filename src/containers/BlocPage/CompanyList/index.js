332e7de072f9d92f12ebeb59e90ed0cdd171239a
GzwNice <gzwnice@163.com>
2018-12-29 18:06:07 +0800

    feat: 调整页面跳转a链接

--- src/containers/BlocPage/CompanyList/index.js
+++ src/containers/BlocPage/CompanyList/index.js
@@ -2,152 +2,58 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import { blocList, blocCategory } from '../../../actions/company'
+import companyLogo from '../../../static/detailLogo.png'
 import style from '../style.less'
 
 @connect(state => {
   return {
-    
+    list: state.company.list,
+    pager: state.company.pager,
   }
 })
 class CompanyList extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state= {
-      companyLogo:
-      'https://p3-v.veimg.cn/sysimg/20180628/55465e8e4fc72ac2f821ad0cfac439af.jpg',
+    this.state = {
     }
   }
-  
-  componentDidMount(){
-    const c_userid = this.props.match.params.c_userid
-    this.props.dispatch(
-      blocList({
-        // page: 1,
-        // pageSize: 10,
-        c_userid: c_userid,
 
-      })).then(data=>{
-        console.log(data);
+  componentDidMount() {
+    const c_userid = this.props.match.params.c_userid
+    this.props
+      .dispatch(
+        blocList({
+          c_userid: c_userid,
+        })
+      )
+      .then(data => {
+        console.log(data)
       })
   }
 
   render() {
-    const { companyLogo } = this.state
+    const list = this.props.list || []
+    console.log(list)
     return (
       <div className={style.companyList}>
-        <div className={style.ContentModule}>
-          <img src={companyLogo} alt="" />
-          <div className={style.inviteInfo}>
-              <h1>杭州洲际酒酒店际酒店际酒店酒店</h1>
-              <div className={style.scale}>
-                <span>杭州</span>
-                <span>国际高端酒店/5星级</span>
-                <span>500-999人</span>
+        {list.map((i, n) => {
+          return (
+            <div className={style.ContentModule} key={n}>
+              <img src={i.company_logo ? i.company_logo :companyLogo} alt="img" />
+              <div className={style.inviteInfo}>
+                <h1>{i.company_name}</h1>
+                <div className={style.scale}>
+                  <span>{i.current_location}</span>
+                  <span>{i.company_type}</span>
+                  <span>{i.employees_number}</span>
+                </div>
+                <div className={style.inRecruit}>
+                  <span>{i.jobNum}</span>个在招职位
+                </div>
               </div>
-              <div className={style.inRecruit}>
-                <span>145</span>个在招职位
-              </div>
-          </div>
-        </div>
-        <div className={style.ContentModule}>
-          <img src={companyLogo} alt="" />
-          <div className={style.inviteInfo}>
-              <h1>杭州洲际酒酒店际酒店际酒店酒店</h1>
-              <div className={style.scale}>
-                <span>杭州</span>
-                <span>国际高端酒店/5星级</span>
-                <span>500-999人</span>
-              </div>
-              <div className={style.inRecruit}>
-                <span>145</span>个在招职位
-              </div>
-          </div>
-        </div>
-        <div className={style.ContentModule}>
-          <img src={companyLogo} alt="" />
-          <div className={style.inviteInfo}>
-              <h1>杭州洲际酒酒店际酒店际酒店酒店</h1>
-              <div className={style.scale}>
-                <span>杭州</span>
-                <span>国际高端酒店/5星级</span>
-                <span>500-999人</span>
-              </div>
-              <div className={style.inRecruit}>
-                <span>145</span>个在招职位
-              </div>
-          </div>
-        </div>
-        <div className={style.ContentModule}>
-          <img src={companyLogo} alt="" />
-          <div className={style.inviteInfo}>
-              <h1>杭州洲际酒酒店际酒店际酒店酒店</h1>
-              <div className={style.scale}>
-                <span>杭州</span>
-                <span>国际高端酒店/5星级</span>
-                <span>500-999人</span>
-              </div>
-              <div className={style.inRecruit}>
-                <span>145</span>个在招职位
-              </div>
-          </div>
-        </div>
-        <div className={style.ContentModule}>
-          <img src={companyLogo} alt="" />
-          <div className={style.inviteInfo}>
-              <h1>杭州洲际酒酒店际酒店际酒店酒店</h1>
-              <div className={style.scale}>
-                <span>杭州</span>
-                <span>国际高端酒店/5星级</span>
-                <span>500-999人</span>
-              </div>
-              <div className={style.inRecruit}>
-                <span>145</span>个在招职位
-              </div>
-          </div>
-        </div>
-        <div className={style.ContentModule}>
-          <img src={companyLogo} alt="" />
-          <div className={style.inviteInfo}>
-              <h1>杭州洲际酒酒店际酒店际酒店酒店</h1>
-              <div className={style.scale}>
-                <span>杭州</span>
-                <span>国际高端酒店/5星级</span>
-                <span>500-999人</span>
-              </div>
-              <div className={style.inRecruit}>
-                <span>145</span>个在招职位
-              </div>
-          </div>
-        </div>
-        <div className={style.ContentModule}>
-          <img src={companyLogo} alt="" />
-          <div className={style.inviteInfo}>
-              <h1>杭州洲际酒酒店际酒店际酒店酒店</h1>
-              <div className={style.scale}>
-                <span>杭州</span>
-                <span>国际高端酒店/5星级</span>
-                <span>500-999人</span>
-              </div>
-              <div className={style.inRecruit}>
-                <span>145</span>个在招职位
-              </div>
-          </div>
-        </div>
-        <div className={style.ContentModule}>
-          <img src={companyLogo} alt="" />
-          <div className={style.inviteInfo}>
-              <h1>杭州洲际酒酒店际酒店际酒店酒店</h1>
-              <div className={style.scale}>
-                <span>杭州</span>
-                <span>国际高端酒店/5星级</span>
-                <span>500-999人</span>
-              </div>
-              <div className={style.inRecruit}>
-                <span>145</span>个在招职位
-              </div>
-          </div>
-        </div>
-        
+            </div>
+          )
+        })}
       </div>
     )
   }
