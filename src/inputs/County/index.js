7c466210edd9bebfe2afb4187c8ffd08852c9dc7
zhangji <zhangji@dfwsgroup.com>
2019-01-08 11:36:00 +0800

    county

--- src/inputs/County/index.js
+++ src/inputs/County/index.js
@@ -1,98 +1,83 @@
-import React from 'react'
-import ComplexFormField from '../../../components/Complex/ComplexFormField'
-import { NavBar, Toast, Accordion, List, Icon, InputItem } from 'antd-mobile'
+import React, { Component } from 'react'
+import {
+  NavBar,
+  Toast,
+  Accordion,
+  List,
+  Icon,
+  InputItem,
+  Modal,
+} from 'antd-mobile'
 import style from './style.less'
 import { connect } from 'react-redux'
-import { getCompanyTips } from "../../../actions/work_exps";
+import checkeIcon from '../../static/checked@3x.png'
 
 @connect(state => {
   return {
-    dataList: state.work_exps.companyTipsList,
+    options: state.option,
   }
 })
-class ComplexSelView extends ComplexFormField {
+class County extends Component {
   constructor(props) {
     super(props)
-    // console.log(this.props)
     this.state = {
-      value: '',
-      show: false,
+      visible: false,
+      country: '0086',
+      name: '中国大陆',
     }
   }
-
-  save = () => {
-    const { value } = this.state
-    if (value.length > 0) {
-      this.props.onChange(value)
-      this.changeVisible(false, true)
-    } else {
-      Toast.info('请输入内容', 2)
-    }
-  }
-  onChange = (value) => {
-    const parmas = {
-      keyword: value,
-    }
-    this.props.dispatch(getCompanyTips(parmas)).then(() => {
-      this.setState({
-        value,
-        show: true,
-      })
-    })
-
-  }
-  handleClick = (index) => {
-    const { dataList } = this.props
-    this.props.onChange(dataList[index])
-    this.changeVisible(false, true)
+  goback = () => {
     this.setState({
-      show: false,
+      visible: false,
     })
   }
-  mainView() {
-    const { dataList = [] } = this.props
-    const defaultValue = this.props.value
-    const { value, show } = this.state
-    let arr = []
-    dataList.map((item,index) => {
-      let re =new RegExp(value,"g"); //定义正则
-      item = item.replace(re, `<span>${value}</span>`); //进行替换，并定义高亮的样式
-      arr.push(item)
+  handleClick = item => {
+    this.setState({
+      name: item.name,
+      country: item.country,
+      visible: false,
     })
-
+    this.props.setSet(item)
+  }
+  handleShow = () => {
+    this.setState({
+      visible: true,
+    })
+  }
+  render() {
+    const { opt_phone_rules = [] } = this.props.options
+    const { visible, country, name } = this.state
+    console.log(opt_phone_rules)
     return (
-      <div className={style.root}>
-        <NavBar
-          mode="light"
-          className={style.nav}
-          icon={<Icon type="left" />}
-          onLeftClick={() => this.changeVisible()}
-          rightContent={<span onClick={() => this.save()}>保存</span>}>
-          所在公司
-        </NavBar>
-        <div className={style.search}>
-          <InputItem
-            clear
-            defaultValue={defaultValue}
-            placeholder="请输入职位名称"
-            onChange={this.onChange}
+      <div>
+        <span onClick={this.handleShow} className={style.label}>
+          {country} <Icon type="down" />
+        </span>
+        <Modal visible={visible}>
+          <NavBar
+            mode="light"
+            className={style.nav}
+            icon={<Icon type="left" />}
+            onLeftClick={() => this.goback()}
           />
-        </div>
-        <ul className={`${style.list} ${show ? style.show : style.hide}`} >
-          {
-            arr.map((item, index) => {
+          <div className={style.title}>请选择国家或地区</div>
+          <ul className={style.content}>
+            {opt_phone_rules.map((item, index) => {
               return (
-                <li
-                  key={index}
-                  onClick={this.handleClick.bind(this, index)}
-                  dangerouslySetInnerHTML={{__html: item}} />
+                <li key={index} onClick={this.handleClick.bind(this, item)}>
+                  <div>{item.name}</div>
+                  <div>{item.country}</div>
+                  <div>
+                    {country === item.country ? <img src={checkeIcon} /> : null}
+                  </div>
+                </li>
               )
-            })
-          }
-        </ul>
+            })}
+          </ul>
+        </Modal>
       </div>
     )
   }
 }
 
-export default ComplexSelView
+export default County
