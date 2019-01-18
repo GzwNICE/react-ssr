b5c731713d8827485883aabb62d00357930a6c83
zhangji <zhangji@dfwsgroup.com>
2019-01-18 17:58:33 +0800

    工作

---
+++ src/components/Time/startTime.js
@@ -0,0 +1,103 @@
+import React, { PureComponent } from 'react'
+import { DatePicker } from 'antd-mobile'
+import style from './style.less'
+import moment from 'moment'
+import { connect } from 'react-redux'
+
+const nowYear = moment().weekYear()
+const maxDate = new Date()
+const minDate = moment().year(nowYear - 80).month(0)._d
+let timeChange = false
+const CustomChildren = ({ extra, onClick, children }) => {
+  extra = timeChange ? extra : '请选择'
+  console.log(extra)
+  return (
+    <div
+      onClick={onClick}
+      style={{
+        backgroundColor: '#fff',
+        height: '50px',
+        lineHeight: '50px',
+        padding: '0 20px',
+        color: '#4A4A4A',
+      }}
+    >
+      {children}
+      <div className={style.rightIcon} aria-hidden="true" />
+      <span style={{ float: 'right', color: '#888' }}>{extra}</span>
+    </div>
+  )
+}
+
+@connect(state => {
+  return {}
+})
+class JobTime extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: moment().year(nowYear - 1)._d,
+    }
+  }
+  componentDidMount() {
+    timeChange = false
+    console.log(timeChange)
+    const { value } = this.props
+    this.initVal(value)
+  }
+  componentWillReceiveProps(next) {
+    const { value } = next
+    this.initVal(value)
+  }
+  initVal = (value) => {
+    if (
+      value !== undefined &&
+      value &&
+      typeof value === 'string' &&
+      value.indexOf('-') !== -1
+    ) {
+      let dt = new Date(value.replace(/-/, '/'))
+      timeChange = true
+      this.setState({
+        value: dt,
+      })
+      const onChange = this.props.onChange
+      console.log(timeChange)
+      if (onChange) {
+        onChange(dt)
+      }
+    }
+  }
+  onChange = value => {
+    this.setState({
+      value,
+    })
+    const onChange = this.props.onChange
+
+    if (onChange) {
+      onChange(value)
+    }
+    timeChange = true
+  }
+
+  render() {
+    const { value } = this.state
+    const { title } = this.props
+    return (
+      <DatePicker
+        mode="month"
+        title={title}
+        extra="请选择"
+        format={s => moment(s).format('YYYY.MM')}
+        minDate={minDate}
+        maxDate={maxDate}
+        value={value}
+        onChange={this.onChange}
+      >
+        <CustomChildren>{title}</CustomChildren>
+      </DatePicker>
+    )
+  }
+}
+
+export default JobTime
