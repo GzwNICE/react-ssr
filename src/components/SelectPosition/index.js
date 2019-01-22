02398629afe9b9241391d01c19ace07a57bad694
zhangji <zhangji@dfwsgroup.com>
2019-01-22 17:54:17 +0800

    简历

--- src/components/SelectPosition/index.js
+++ src/components/SelectPosition/index.js
@@ -24,7 +24,7 @@ class ComplexSelView extends ComplexFormField {
   }
   componentDidMount() {
     const { options } = this.props
-    console.log(this.props)
+    // console.log(this.props)
     this.setState({
       optionsSubChil: options[0].sublist,
     })
